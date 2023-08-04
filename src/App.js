7b826cba15abc7c9d8643008d8d8380b6e385d6e
Winfred <2508649869@qq.com>
2023-08-04 23:35:13 +0800

    add Echart rely

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React,{Component} from "react";
 
+const flag=false;
+const obj=[
+    {id:1,content:"fd"},
+    {id:2,content:"dd"},
+    {id:3,content:"fdas"},
+]
+
 export default class App extends Component{
     render(){
         return(
-            <div>
-                
+            <div className="gridMother">
+                 <div className="mainBoard">
+                    三元表达式
+                    <br></br>
+                    {flag ?"hahah":"444"}
+                    <div className="timeLine">
+                    <ul>
+                    循环渲染
+                    {obj.map(({id,content})=>
+                        (<li key={id}>
+                            {id}|{content}
+                        </li>)
+                        )
+                    }
+                    </ul>
+                    条件渲染
+                    </div>
+                 </div>
+                 <div className="assistBoard">
+                 </div>
             </div>
-        )
+        );
     }
 }
